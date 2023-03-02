f552a46ed16e0a1ca4cf4a944aaccaa07badde1c
butebaliyeva <utebaliyevabotakoz@gmail.com>
2023-03-03 01:33:22 +0600

    Android Wiki

---
+++ src/test/java/tests/android/selenide/SearchTests.java
@@ -0,0 +1,25 @@
+package tests.android.selenide;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.appium.java_client.AppiumBy.accessibilityId;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.id;
+
+public class SearchTests extends TestBase {
+
+    @Test
+    void searchTest() {
+        step("Type search", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
+        });
+        step("Verify content found", () ->
+                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
+    }
+}
