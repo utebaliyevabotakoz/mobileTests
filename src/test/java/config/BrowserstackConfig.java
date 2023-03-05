d4ca633ee772c395fe3f7cd1cd56c2a505525896
butebaliyeva <utebaliyevabotakoz@gmail.com>
2023-03-05 17:14:40 +0600

    Android finish

---
+++ src/test/java/config/BrowserstackConfig.java
@@ -0,0 +1,42 @@
+package config;
+
+
+import org.aeonbits.owner.Config;
+
+@BrowserstackConfig.LoadPolicy(BrowserstackConfig.LoadType.MERGE)
+@org.aeonbits.owner.Config.Sources(
+        {"system:properties",
+                "classpath:${env}.properties",
+                "classpath:secret.properties"})
+
+
+public interface BrowserstackConfig extends Config {
+
+    @Key("login")
+    String login();
+
+    @Key("password")
+    String password();
+
+    @Key("baseURL")
+    String baseUrl();
+
+    @Key("appURL")
+    String appUrl();
+
+    @Key("device")
+    String device();
+
+    @Key("os_version")
+    String osVersion();
+
+    @Key("project")
+    String projectName();
+
+    @Key("build")
+    String buildName();
+
+    @Key("name")
+    String testName();
+
+}
--- src/test/java/drivers/BrowserstackDriver.java
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -1,10 +1,13 @@
 package drivers;
 
 import com.codeborne.selenide.WebDriverProvider;
+import config.BrowserstackConfig;
 import io.appium.java_client.android.AndroidDriver;
 import lombok.SneakyThrows;
+import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.MutableCapabilities;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -14,30 +17,32 @@ import java.net.URL;
 
 public class BrowserstackDriver implements WebDriverProvider {
 
+    static BrowserstackConfig config = ConfigFactory.create(BrowserstackConfig.class);
     @SneakyThrows
+    @Nonnull
     @Override
     public WebDriver createDriver(@Nonnull Capabilities capabilities) {
         MutableCapabilities mutableCapabilities = new MutableCapabilities();
         mutableCapabilities.merge(capabilities);
 
         // Set your access credentials
-        mutableCapabilities.setCapability("browserstack.user", "botakozutebaliye_FN8sZr");
-        mutableCapabilities.setCapability("browserstack.key", "yZA6t6mFy4Nh9fBvrqzs");
+        mutableCapabilities.setCapability("browserstack.user", config.login());
+        mutableCapabilities.setCapability("browserstack.key", config.password());
 
         // Set URL of the application under test
-        mutableCapabilities.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+        mutableCapabilities.setCapability("app", config.appUrl());
 
         // Specify device and os_version for testing
-        mutableCapabilities.setCapability("device", "Google Pixel 3");
-        mutableCapabilities.setCapability("os_version", "9.0");
+        mutableCapabilities.setCapability("device", config.device());
+        mutableCapabilities.setCapability("os_version", config.osVersion());
 
         // Set other BrowserStack capabilities
-        mutableCapabilities.setCapability("project", "First Java Project");
-        mutableCapabilities.setCapability("build", "browserstack-build-1");
-        mutableCapabilities.setCapability("name", "first_test");
+        mutableCapabilities.setCapability("project", config.projectName());
+        mutableCapabilities.setCapability("build", config.buildName());
+        mutableCapabilities.setCapability("name", config.testName());
 
         // Initialise the remote Webdriver using BrowserStack remote URL
         // and desired capabilities defined above
-        return new RemoteWebDriver(new URL("http://hub.browserstack.com/wd/hub"), mutableCapabilities);
+        return new RemoteWebDriver(new URL(config.baseUrl()), mutableCapabilities);
     }
 }
